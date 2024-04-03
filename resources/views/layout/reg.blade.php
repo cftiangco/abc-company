6df1683211eb1f2acfd632fc3fe28e901e20ed70
Crimson Tiangco <crimson.tiangco.ipi@prulifeuk.com.ph>
2024-04-03 19:43:03 +0800

    fix builded css

--- resources/views/layout/reg.blade.php
+++ resources/views/layout/reg.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="{{ asset('images/iconn.png') }}">
   <title>ABC Company @yield('title')</title>
-  <link rel="stylesheet" href="{{ asset('build/assets/app-sk0mS2Z4.css') }}" />
+  <link rel="stylesheet" href="{{ asset('build/assets/app-CWdtRgTb.css') }}" />
 </head>
 <body>
       <div class="h-screen">
